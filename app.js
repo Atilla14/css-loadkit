277d37683e2a9a0bd9acc125f1d8754d94e6c861
atilla_14 <sathyanarayanravi@gmail.com>
2016-10-04 05:15:10 +0530

    Fix fatal error in express server

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ app.set('port', process.env.PORT || 5000)
 
 app.use(express.static(path.join(__dirname, "public")))
 
-app.listen(port, function () {
-  console.log('Listening on port ' + port + '...')
+app.listen(app.get(port), function () {
+  console.log('Listening on port ' + app.get(port) + '...')
 })
 
 app.get('/', function (req, res) {
