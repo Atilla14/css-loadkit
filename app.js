c33581783ae0e3a454e61d63a9c73535975e460c
atilla_14 <sathyanarayanravi@gmail.com>
2016-10-04 04:34:48 +0530

    Convert app to es5 for node 5.11 on heroku

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-let express = require('express')
-let path = require('path')
-let app = express()
-let port = 5000
+var express = require('express')
+var path = require('path')
+var app = express()
+var port = 5000
 
 app.use(express.static(path.join(__dirname, "public")))
 
-app.listen(port, () => {
+app.listen(port, function () {
   console.log('Listening on port ' + port + '...')
 })
 
-app.get('/', (req, res) => {
+app.get('/', function (req, res) {
   res.send('index.html')
 })
